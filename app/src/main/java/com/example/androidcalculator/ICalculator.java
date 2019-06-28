0ee3d7eb9ffc62d3f922582e5c1a0739f4cfde9a
Partekaraz <aazor@covalco.es>
2019-06-28 11:10:31 +0200

    Cerrar issue close #4

---
+++ app/src/main/java/com/example/androidcalculator/ICalculator.java
@@ -0,0 +1,8 @@
+package com.example.androidcalculator;
+
+public interface ICalculator {
+    int add();
+    int substract();
+    int multiply();
+    int divide();
+}
--- app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.androidcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnDivide;
+    private EditText txtNum1;
+    private EditText txtNum2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDivide =(Button)findViewById(R.id.btnDividir);
+        txtNum1 = (EditText) findViewById(R.id.txtNum1);
+        txtNum2 =  (EditText) findViewById(R.id.txtNum2);
+    }
+
+    public void onClickDivide(View view){
+
+        int num1 = Integer.parseInt(txtNum1.getText().toString());
+        int num2 = Integer.parseInt(txtNum2.getText().toString());
+        ICalculator iCalculator = new Calculator(num1, num2);
+        int result =iCalculator.divide();
+        Toast.makeText(getApplicationContext(),
+                "The result of add is " + result,
+                Toast.LENGTH_LONG).show();
     }
 }
